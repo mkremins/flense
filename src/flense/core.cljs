7bc2385854582ffe027828603b4a303e32647613
Max Kreminski <maxkreminski@gmail.com>
2014-04-07 09:00:36 -0700

    Majorly overhaul control scheme for smoother editing experience

--- src/flense/core.cljs
+++ src/flense/core.cljs
@@ -9,177 +9,120 @@
 
 (def selected (atom ($ ".selected")))
 
-(defn select! [$elem]
-  (.removeClass @selected "selected")
-  (reset! selected $elem)
-  (.addClass $elem "selected"))
-
-(defn exists? [$elem]
-  (> (.-length $elem) 0))
-
-;; edit commands
-
-(declare enable-edit-mode! go-down! go-right! go-up!)
-
-(defn insert-token! []
-  (let [$selected @selected]
-    (.after $selected "<span class=\"token\"></span>")
-    (go-right!)
-    (enable-edit-mode!)))
-
-(defn delete-selected! []
-  (let [$deleted @selected]
-    (go-right!)
-    (when (= @selected $deleted)
-      (go-up!))
-    (.remove $deleted)))
-
-;; edit mode management
-
-(def edit-mode? (atom false))
-
-(defn select-text! [elem]
+(defn- select-text! [elem]
   (let [text-range     (.createRange js/document)
         text-selection (.getSelection js/window)]
     (.selectNodeContents text-range elem)
     (.removeAllRanges text-selection)
     (.addRange text-selection text-range)))
 
-(defn enable-edit-mode! []
-  (let [$selected @selected]
-    (when (.hasClass $selected "token")
-      (reset! edit-mode? true)
-      (.attr $selected "contenteditable" true)
-      (let [elem (.get $selected 0)]
-        (.focus elem)
-        (select-text! elem)))
-    (when (.hasClass $selected "coll")
-      (go-down!)
-      (enable-edit-mode!))))
+(defn enable-editing! [$elem]
+  (when (.hasClass $elem "token")
+    (.attr $elem "contenteditable" true)
+    (let [elem (.get $elem 0)]
+      (.focus elem)
+      (select-text! elem))))
 
-(defn disable-edit-mode! []
-  (let [$selected @selected]
-    (when (.hasClass $selected "token")
-      (reset! edit-mode? false)
-      (.removeAttr $selected "contenteditable")
-      (.blur (.get $selected 0))
-      (when-not (seq (.text $selected))
-        (delete-selected!)))))
+(defn disable-editing! [$elem]
+  (when (.hasClass $elem "token")
+    (.removeAttr $elem "contenteditable")
+    (.blur (.get $elem 0))))
+
+(defn select! [$elem]
+  (let [$prev @selected]
+    (disable-editing! $prev)
+    (when (and (.hasClass $prev "token") (empty? (.text $prev)))
+      (.remove $prev))
+    (.removeClass $prev "selected"))
+  (reset! selected $elem)
+  (.addClass $elem "selected")
+  (when (.hasClass $elem "token")
+    (enable-editing! $elem)))
+
+(defn exists? [$elem]
+  (> (.-length $elem) 0))
 
 ;; zipper navigation commands
 
 (defn go-down! []
   (let [$selected @selected]
     (when (.hasClass $selected "coll")
-      (let [$items (.children $selected ".items")]
+      (let [$items (.children $selected)]
         (when (exists? $items)
-          (select! (-> $items (.children) (.first))))))))
+          (select! (.first $items)))))))
 
 (defn go-up! []
   (let [$selected @selected]
     (when-not (.hasClass $selected "top")
-      (select! (-> $selected (.parent) (.parent))))))
+      (select! (.parent $selected)))))
 
 (defn go-left! []
-  (let [$selected @selected]
-    (when-not (.hasClass $selected "top")
-      (let [$prev (.prev $selected)]
-        (if (exists? $prev)
-            (select! $prev)
-            (let [$last (-> $selected (.siblings) (.last))]
-              (when (exists? $last)
-                (select! $last))))))))
+  (let [$selected @selected
+        $prev     (.prev $selected)]
+    (if (exists? $prev)
+        (select! $prev)
+        (let [$last (-> $selected (.siblings) (.last))]
+          (when (exists? $last)
+            (select! $last))))))
 
 (defn go-right! []
+  (let [$selected @selected
+        $next     (.next $selected)]
+    (if (exists? $next)
+        (select! $next)
+        (let [$first (-> $selected (.siblings) (.first))]
+          (when (exists? $first)
+            (select! $first))))))
+
+;; structure editing commands
+
+(defn- render-token []
+  "<span class=\"token\">...</span>")
+
+(defn- render-coll [type]
+  (str "<div class=\"coll " (name type) "\">"
+       (render-token)
+       "</div>"))
+
+(defn open-coll! [type]
   (let [$selected @selected]
-    (when-not (.hasClass $selected "top")
-      (let [$next (.next $selected)]
-        (if (exists? $next)
-            (select! $next)
-            (let [$first (-> $selected (.siblings) (.first))]
-              (when (exists? $first)
-                (select! $first))))))))
+    (.after $selected (render-coll type))
+    (go-right!)))
 
-(defn walk-down! []
-  (loop [$selected @selected]
-    (when (and (.hasClass $selected "coll")
-               (exists? (.children $selected ".items")))
-        (go-down!)
-        (recur @selected))))
-
-(defn walk-right! []
+(defn break-token! []
   (let [$selected @selected]
-    (when-not (.hasClass $selected "top")
-      (let [$next (.next $selected)]
-        (if (exists? $next)
-            (select! $next)
-            (do (go-up!)
-                (go-right!)
-                (walk-down!)))))))
+    (.after $selected (render-token))
+    (go-right!)))
 
-;; positional navigation commands
-
-(defn go-to-child! [n]
-  (let [$selected @selected]
-    (when (.hasClass $selected "coll")
-      (let [$child (-> $selected
-                       (.find (str ".items > :nth-child(" (inc n) ")" ))
-                       (.first))]
-        (when (exists? $child)
-          (select! $child))))))
-
-(defn go-to-sibling! [n]
-  (let [$sibling (.children (.parent @selected)
-                            (str ":nth-child(" (inc n) ")"))]
-    (when (exists? $sibling)
-      (select! $sibling))))
+(defn delete-selected! []
+  (let [$deleted @selected]
+    (go-left!)
+    (when (= @selected $deleted)
+      (go-up!))
+    (.remove $deleted)))
 
 ;; keybinds
 
 (def default-binds
-  { ;; tree structure editing commands
-   :DEL   #(do (.preventDefault %) (delete-selected!))
-   :ENTER #(do (.preventDefault %) (enable-edit-mode!))
-   :SPACE #(do (.preventDefault %) (insert-token!))
-    ;; standard zipper-walking navigation commands
+  { ;; structural editing commands
+   :DEL             delete-selected!
+   :LBRAK           (partial open-coll! :vec)
+   :SPACE           break-token!
+   #{:SHIFT :NUM_9} (partial open-coll! :seq)
+   #{:SHIFT :LBRAK} (partial open-coll! :map)
+    ;; simple navigation commands
    :DOWN  go-down!
    :LEFT  go-left!
    :RIGHT go-right!
-   :UP    go-up!
-    ;; jump to nth child of selected coll
-   :NUM_1 (partial go-to-child! 0)
-   :NUM_2 (partial go-to-child! 1)
-   :NUM_3 (partial go-to-child! 2)
-   :NUM_4 (partial go-to-child! 3)
-   :NUM_5 (partial go-to-child! 4)
-   :NUM_6 (partial go-to-child! 5)
-   :NUM_7 (partial go-to-child! 6)
-   :NUM_8 (partial go-to-child! 7)
-    ;; jump to nth sibling of selected node
-   #{:CTRL :NUM_1} (partial go-to-sibling! 0)
-   #{:CTRL :NUM_2} (partial go-to-sibling! 1)
-   #{:CTRL :NUM_3} (partial go-to-sibling! 2)
-   #{:CTRL :NUM_4} (partial go-to-sibling! 3)
-   #{:CTRL :NUM_5} (partial go-to-sibling! 4)
-   #{:CTRL :NUM_6} (partial go-to-sibling! 5)
-   #{:CTRL :NUM_7} (partial go-to-sibling! 6)
-   #{:CTRL :NUM_8} (partial go-to-sibling! 7)})
+   :UP    go-up!})
 
-(def edit-mode-binds
-  { ;; synonyms for "save changes and stop editing"
-   :ENTER #(do (.preventDefault %) (disable-edit-mode!))
-   :ESC   #(do (.preventDefault %) (disable-edit-mode!))
-   :UP    #(do (.preventDefault %) (disable-edit-mode!))
-    ;; mode-specific navigation commands
-   :TAB   #(do (.preventDefault %) (walk-right!) (enable-edit-mode!))})
-
-(def modal-keys #{:CTRL})
+(def modal-keys #{:ALT :CTRL :SHIFT})
 
 (defn handle-key [ev]
-  (let [binds (if @edit-mode? edit-mode-binds default-binds)
-        keys  (->> modal-keys (filter held?) (reduce conj #{(ev->key ev)}))
-        keys  (if (= (count keys) 1) (first keys) keys)]
-    (when-let [exec-bind! (get binds keys)]
+  (let [keys (->> modal-keys (filter held?) (reduce conj #{(ev->key ev)}))
+        keys (if (= (count keys) 1) (first keys) keys)]
+    (when-let [exec-bind! (get default-binds keys)]
+      (.preventDefault ev)
       (exec-bind! ev))))
 
 ;; application setup and wiring
--- src/flense/keys.cljs
+++ src/flense/keys.cljs
@@ -8,26 +8,30 @@
 (def key-name
   "Given an integer keycode, returns a human-readable keyword naming the
    corresponding key."
-  {17 :CTRL 
-    8 :DEL
-   40 :DOWN
-   13 :ENTER
-   27 :ESC
-   37 :LEFT
-   49 :NUM_1
-   50 :NUM_2
-   51 :NUM_3
-   52 :NUM_4
-   53 :NUM_5
-   54 :NUM_6
-   55 :NUM_7
-   56 :NUM_8
-   57 :NUM_9
-   48 :NUM_0
-   39 :RIGHT
-   32 :SPACE
-    9 :TAB
-   38 :UP})
+  { 18 :ALT
+    17 :CTRL
+     8 :DEL
+    40 :DOWN
+    13 :ENTER
+    27 :ESC
+   219 :LBRAK
+    37 :LEFT
+    49 :NUM_1
+    50 :NUM_2
+    51 :NUM_3
+    52 :NUM_4
+    53 :NUM_5
+    54 :NUM_6
+    55 :NUM_7
+    56 :NUM_8
+    57 :NUM_9
+    48 :NUM_0
+   221 :RBRAK
+    39 :RIGHT
+    16 :SHIFT
+    32 :SPACE
+     9 :TAB
+    38 :UP})
 
 (def ev->key
   "Given a key event, returns a human-readable keyword naming the associated
