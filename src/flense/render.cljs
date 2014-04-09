adcd66d8992180cbd38bd4eaf01d81447f9c8e00
Max Kreminski <maxkreminski@gmail.com>
2014-04-09 08:40:19 -0700

    Make complex component rendering more idiomatic and performant

--- src/flense/render.cljs
+++ src/flense/render.cljs
@@ -1,5 +1,7 @@
 (ns flense.render
-  (:require [clojure.zip :as zip]))
+  (:require [clojure.zip :as zip]
+            [flense.ranges :as ranges]
+            [reagent.core :as reagent]))
 
 (defn- right-locs [loc]
   (loop [loc loc locs []]
@@ -19,27 +21,37 @@
 
 (declare render)
 
-(defn- render-coll [type children selected?]
+(defn- render-coll [{:keys [type children selected?]}]
   (let [classes (str (name type) (when selected? " selected"))]
     [:div.coll {:class classes} children]))
 
-(defn- render-token [text selected?]
-  [:span.token (if selected? {:class "selected"} {}) text])
+(defn- render-token [props]
+  (letfn [(do-render [{:keys [text selected?]}]
+            [:span.token (merge {:content-editable true}
+                                (when selected? {:class "selected"}))
+                         text])]
+    (with-meta do-render
+               {:component-did-update
+                (fn [this]
+                  (let [dom-node (reagent/dom-node this)]
+                    (when (.contains (.-classList dom-node) "selected")
+                      (.focus dom-node)
+                      (ranges/select-contents! dom-node))))})))
 
 (defn- render [loc curr-loc]
   (let [node (zip/node loc)
         selected? (= loc curr-loc)]
     (if (coll? node)
-        (render-coll (cond (map? node) :map
-                           (seq? node) :seq
-                           (set? node) :set
-                           (vector? node) :vec)
-                     (map #(render % curr-loc) (downs loc))
-                     selected?)
-        (render-token (if (string? node)
-                          (str "\"" node "\"")
-                          (str node))
-                      selected?))))
+        [render-coll {:type (cond (map? node) :map
+                                  (seq? node) :seq
+                                  (set? node) :set
+                                  (vector? node) :vec)
+                      :children (map #(render % curr-loc) (downs loc))
+                      :selected? selected?}]
+        [render-token {:text (if (string? node)
+                                 (str "\"" node "\"")
+                                 (str node))
+                       :selected? selected?}])))
 
 (defn root [state]
   (let [curr-loc @state]
