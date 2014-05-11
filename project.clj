b0aeca6a625d19faf9ae0acffbb83bfa28a8f6f7
Max Kreminski <maxkreminski@gmail.com>
2014-05-10 23:47:21 -0700

    Bump dependency versions
    Om 0.6.1 => 0.6.2

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   [[org.clojure/clojure "1.6.0"]
    [org.clojure/clojurescript "0.0-2202"]
    [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]
-   [om "0.6.1"]
+   [om "0.6.2"]
    [mkremins/fs "0.2.0"]]
 
   :plugins
