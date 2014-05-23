3632466822a17c58f54b62199bb37902c4b21078
Max Kreminski <maxkreminski@gmail.com>
2014-05-23 06:10:32 -0400

    Bump dependency versions
    * ClojureScript 0.0-2202 => 0.0-2227

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (defproject flense "0.1.0-SNAPSHOT"
   :dependencies
   [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-2202"]
+   [org.clojure/clojurescript "0.0-2227"]
    [org.clojure/core.async "0.1.298.0-2a82a1-alpha"]
    [om "0.6.2"]
    [mkremins/fs "0.2.0"]
