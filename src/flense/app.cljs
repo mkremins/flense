46300ebb1ddcbc415f7f5dde15852f3a295d9c79
Max Kreminski <maxkreminski@gmail.com>
2014-07-04 15:45:09 -0400

    Cleanup: don't def edit and error chans as dynamic vars

--- src/flense/app.cljs
+++ src/flense/app.cljs
@@ -26,13 +26,13 @@
     :tree {:children
            [(p/form->tree '(fn greet [name] (str "Hello, " name "!")))]}}))
 
-(def ^:dynamic ^:private *edit-chan*)
-(def ^:dynamic ^:private *error-chan*)
+(def ^:private edit-chan (async/chan))
+(def ^:private error-chan (async/chan))
 
 (defn raise!
   "Display error message `message` to the user in the popover error bar."
   [message]
-  (async/put! *error-chan* message))
+  (async/put! error-chan message))
 
 (defn open!
   "Load the source file at `fpath` and open the loaded document, discarding any
@@ -52,7 +52,7 @@
       (.. js/document (getElementById "cli") focus))
     (when-let [keybind (-> ks *keybinds* (@actions))]
       (.preventDefault ev)
-      (async/put! *edit-chan* keybind))))
+      (async/put! edit-chan keybind))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; text commands
@@ -76,20 +76,18 @@
 
 (defn init []
   (set! *keybinds* (p/load-config "resources/config/keymap.edn"))
-  (set! *edit-chan* (async/chan))
-  (set! *error-chan* (async/chan))
   (let [command-chan (async/chan)]
     (hist/push-state! @app-state)
     (om/root editor-view app-state
              {:target (.getElementById js/document "editor-parent")
-              :shared {:edit-chan *edit-chan*}
+              :shared {:edit-chan edit-chan}
               :tx-listen handle-tx})
     (om/root cli-view nil
              {:target (.getElementById js/document "cli-parent")
               :shared {:command-chan command-chan}})
     (om/root error-bar-view nil
              {:target (.getElementById js/document "error-bar-parent")
-              :shared {:error-chan *error-chan*}})
+              :shared {:error-chan error-chan}})
     (go-loop []
       (let [[command & args] (<! command-chan)]
         (apply handle-command command args))
