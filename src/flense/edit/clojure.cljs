75fd1969a5efa4577de85897d65e7b4fdc57cbe0
Max Kreminski <maxkreminski@gmail.com>
2014-07-21 10:04:15 -0400

    Implement proof-of-concept :clojure/expand-macro action

--- src/flense/edit/clojure.cljs
+++ src/flense/edit/clojure.cljs
@@ -1,6 +1,7 @@
 (ns flense.edit.clojure
+  (:refer-clojure :exclude [macroexpand macroexpand-1])
   (:require [flense.edit :refer [action find-placeholder]]
-            [flense.parse :refer [form->tree]]
+            [flense.parse :refer [form->tree tree->form]]
             [flense.util :refer [maybe update]]
             [xyzzy.core :as z]))
 
@@ -26,3 +27,22 @@
 (action :clojure/toggle-dispatch
         :when #(contains? dispatch-types (-> % z/node :type))
         :edit #(z/edit % update :type (partial maybe dispatch-types)))
+
+(def macros
+  {'when (fn [test & body] `(if ~test (do ~@body) nil))})
+
+(defn- macro-form? [form]
+  (and (seq? form) (contains? macros (first form))))
+
+(defn- macroexpand-1 [[sym & args]]
+  (apply (macros sym) args))
+
+(defn- macroexpand [form]
+  (let [expanded (macroexpand-1 form)]
+    (if (macro-form? expanded)
+      (recur expanded)
+      expanded)))
+
+(action :clojure/expand-macro
+        :when #(-> % z/node tree->form macro-form?)
+        :edit #(z/edit % (comp form->tree macroexpand tree->form)))
