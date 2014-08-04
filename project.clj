2a386e1516f337273dc840ffbb4c1e52b32bdd8d
Max Kreminski <maxkreminski@gmail.com>
2014-08-04 10:15:05 -0400

    Use React as an ordinary Leiningen dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   [[org.clojure/clojure "1.6.0"]
    [org.clojure/clojurescript "0.0-2280"]
    [org.clojure/core.async "0.1.303.0-886421-alpha"]
+   [com.facebook/react "0.11.1"]
    [om "0.7.0"]
    [spellhouse/phalanges "0.1.3"]
    [mkremins/fs "0.2.0"]
@@ -16,7 +17,8 @@
   :cljsbuild
   {:builds
    [{:source-paths ["src"]
-     :compiler {:output-to "target/flense.js"
+     :compiler {:preamble ["react/react.js"]
+                :output-to "target/flense.js"
                 :source-map "target/flense.js.map"
                 :optimizations :whitespace
                 :pretty-print true}}]})
