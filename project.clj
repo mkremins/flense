22c815ece0bb6b618c3e4537fb86a36ca7cd2280
Max Kreminski <maxkreminski@gmail.com>
2014-08-04 10:09:34 -0400

    Bump dependency versions
    * ClojureScript 0.0-2268 => 0.0-2280
    * React 0.10.0 => 0.11.1
    * Om 0.6.4 => 0.7.0

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject flense "0.1.0-SNAPSHOT"
   :dependencies
   [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-2268"]
+   [org.clojure/clojurescript "0.0-2280"]
    [org.clojure/core.async "0.1.303.0-886421-alpha"]
-   [om "0.6.4"]
+   [om "0.7.0"]
    [spellhouse/phalanges "0.1.3"]
    [mkremins/fs "0.2.0"]
    [mkremins/xyzzy "0.1.3"]]
