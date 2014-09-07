25be875cee317c8520d9b8238acb0734126d39f3
Max Kreminski <maxkreminski@gmail.com>
2014-09-07 09:13:19 -0700

    Cleanup: drop unused flense.util fns

--- src/flense/util.cljs
+++ src/flense/util.cljs
@@ -1,27 +1,9 @@
 (ns flense.util)
 
-(defn delete
-  "Deletes the item at `idx` in the vector `v`."
-  [v idx]
-  (if (and (= (count v) 1) (= idx 0))
-      (empty v)
-      (vec (concat (subvec v 0 idx) (subvec v (inc idx))))))
-
 (defn exchange
   "Swaps the values at keys `k1` and `k2` in associative data structure `m`."
   [m k1 k2]
   (assoc m k1 (get m k2) k2 (get m k1)))
-
-(defn insert
-  "Inserts `item` into vector `v` at `idx`, pushing whatever was already at
-   `idx` one slot to the right."
-  [v idx item]
-  (apply conj (subvec v 0 idx) item (subvec v idx)))
-
-(defn lconj
-  "Prepends `item` to vector `v`."
-  [v item]
-  (vec (concat [item] v)))
 
 (defn maybe
   "Returns `(f x)` if its value is non-nil, `x` otherwise."
