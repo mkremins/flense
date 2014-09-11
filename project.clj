eb864cc53e14d8ddd081693ea7420a950a83e2ae
Max Kreminski <maxkreminski@gmail.com>
2014-09-11 10:03:51 -0700

    Add description, URL, license info to project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,10 @@
 (defproject mkremins/flense "0.0-278"
+  :description "Experimental structural editor for Clojure code"
+  :url "https://github.com/mkremins/flense"
+  :license {:name "MIT License"
+            :url "http://opensource.org/licenses/MIT"
+            :distribution :repo}
+
   :dependencies
   [[org.clojure/clojure "1.6.0"]
    [org.clojure/clojurescript "0.0-2322"]
