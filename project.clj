4fd51f9d15e9dc785d4d7e8fe87c37186a93bd07
Max Kreminski <maxkreminski@gmail.com>
2014-10-21 17:53:51 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -7,10 +7,10 @@
 
   :dependencies
   [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-2322"]
-   [org.clojure/core.async "0.1.338.0-5c5012-alpha"]
-   [com.facebook/react "0.11.1"]
-   [om "0.7.1"]
+   [org.clojure/clojurescript "0.0-2341"]
+   [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+   [com.facebook/react "0.11.2"]
+   [om "0.7.3"]
    [mkremins/xyzzy "0.3.0"]]
 
   :plugins
