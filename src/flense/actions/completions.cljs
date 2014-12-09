aa0bfeee85622b3655b3d35ee627a6d86fc5f141
Max Kreminski <maxkreminski@gmail.com>
2014-12-09 07:35:23 -0800

    Define more common templates, prev-completion action

--- src/flense/actions/completions.cljs
+++ src/flense/actions/completions.cljs
@@ -4,31 +4,45 @@
             [flense.util :refer [update]]
             [xyzzy.core :as z]))
 
+(defn has-completions? [x]
+  (and (m/atom? x) (seq (:completions (m/unwrap x)))))
+
 (defn complete [loc]
-  (when (and (m/atom? loc) (seq (:completions (z/node loc))))
+  (when (has-completions? loc)
     (let [{:keys [completions selected-completion]} (z/node loc)
           [type form] (nth completions (or selected-completion 0))]
       (cond-> loc :always (z/replace (m/form->tree form))
                   (= type :template) m/next-placeholder))))
 
 (defn next-completion [loc]
-  (when (and (m/atom? loc) (seq (:completions (z/node loc))))
+  (when (has-completions? loc)
     (let [{:keys [completions selected-completion]} (z/node loc)]
       (if (< selected-completion (dec (count completions)))
         (z/edit loc update :selected-completion inc)
         (z/edit loc assoc :selected-completion 0)))))
 
+(defn prev-completion [loc]
+  (when (has-completions? loc)
+    (let [{:keys [completions selected-completion]} (z/node loc)]
+      (if (pos? selected-completion)
+        (z/edit loc update :selected-completion dec)
+        (z/edit loc assoc :selected-completion (dec (count completions)))))))
+
 (defn locals [loc]
   (distinct (map (comp symbol :text z/node) (clj/collect-binding-locs loc))))
 
 (def templates
-  '{"def"   (def ... ...)
-    "defn"  (defn ... [...] ...)
-    "defn-" (defn- ... [...] ...)
-    "fn"    (fn [...] ...)
-    "if"    (if ... ... ...)
-    "let"   (let [... ...] ...)
-    "when"  (when ... ...)})
+  '{"def"       (def ... ...)
+    "defmacro"  (defmacro ... [...] ...)
+    "defn"      (defn ... [...] ...)
+    "defn-"     (defn- ... [...] ...)
+    "fn"        (fn [...] ...)
+    "if"        (if ... ... ...)
+    "if-let"    (if-let [... ...] ... ...)
+    "let"       (let [... ...] ...)
+    "loop"      (loop [... ...] ...)
+    "when"      (when ... ...)
+    "when-let"  (when-let [... ...] ...)})
 
 (defn completions [loc]
   (when (m/atom? loc)
