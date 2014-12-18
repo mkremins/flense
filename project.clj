888554e66e8bbd364612a41f298b55a27d03663c
Max Kreminski <maxkreminski@gmail.com>
2014-12-18 13:54:23 -0500

    CLJS 0.0-2496

--- project.clj
+++ project.clj
@@ -7,7 +7,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-2411"]
+   [org.clojure/clojurescript "0.0-2496"]
    [com.facebook/react "0.11.2"]
    [om "0.7.3"]
    [mkremins/xyzzy "0.3.1"]]
