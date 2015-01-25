4a24ae5c36faae35fd94f1a9f1d2501bb8a5902b
Max Kreminski <maxkreminski@gmail.com>
2015-01-25 10:43:41 -0800

    Clean up dependencies
    * Don't depend directly on React (pulled in via Om)
    * Don't depend on lein-cljsbuild (no standalone builds)
    * Bump ClojureScript to 0.0-2727
    * Bump Om to 0.8.4

--- project.clj
+++ project.clj
@@ -7,22 +7,7 @@
 
   :dependencies
   [[org.clojure/clojure "1.6.0"]
-   [org.clojure/clojurescript "0.0-2665"]
-   [com.facebook/react "0.12.2.1"]
-   [org.om/om "0.8.0"]
+   [org.clojure/clojurescript "0.0-2727"]
+   [org.omcljs/om "0.8.4"]
    [prismatic/om-tools "0.3.10"]
-   [mkremins/xyzzy "0.3.3"]]
-
-  :plugins
-  [[lein-cljsbuild "1.0.4"]]
-
-  :source-paths ["src"]
-
-  :cljsbuild
-  {:builds
-   [{:source-paths ["src"]
-     :compiler {:preamble ["react/react.js"]
-                :output-to "target/flense.js"
-                :source-map "target/flense.js.map"
-                :optimizations :whitespace
-                :pretty-print true}}]})
+   [mkremins/xyzzy "0.3.3"]])
